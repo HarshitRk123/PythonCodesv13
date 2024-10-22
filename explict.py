5100984ef36a73717c56987952ca1ece4130e2a7
Harshit <harsshitrk0120@gmail.com>
2024-10-22 09:56:13 -0700

    Add files via upload

---
+++ explict.py
@@ -0,0 +1,7 @@
+a = "1001011"  
+b = int(a,5)  
+print ("following the conversion to integer base 5: ", end="")  
+print (b)    
+d = float(a)  
+print ("After converting to float : ", end="")  
+print (d)  
